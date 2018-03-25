678cbfb436db07e4c3822db52f6e641660aa1a88
engramar <engramar.bollas@gmail.com>
2018-03-25 12:34:17 +1100

    first commit

---
+++ app.py
@@ -0,0 +1,86 @@
+from flask import Flask, render_template, g, request, session, redirect, url_for
+from database import get_db
+from werkzeug.security import generate_password_hash, check_password_hash
+import os
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = os.urandom(24)
+
+@app.teardown_appcontext
+def close_db(error):
+    if hasattr(g, 'sqlite_db'):
+        g.sqlite_db.close()
+
+def get_current_user():
+    user_result = None
+
+    if 'user' in session:
+        user = session['user']
+
+        db = get_db()
+        user_cur = db.execute('select id, name, password, expert, admin from users where name = ?', [user])
+        user_result = user_cur.fetchone()
+
+    return user_result
+
+@app.route('/')
+def index():
+    user = get_current_user()
+    return render_template('home.html', user=user)
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    user = get_current_user()
+
+    if request.method == 'POST':
+
+        db = get_db()
+        existing_user_cur = db.execute('select id from users where name = ?', [request.form['name']])
+        existing_user = existing_user_cur.fetchone()
+
+        if existing_user:
+            return render_template('register.html', user=user, error='User already exists!')
+
+        hashed_password = generate_password_hash(request.form['password'], method='sha256')
+        db.execute('insert into users (name, password, expert, admin) values (?, ?, ?, ?)', [request.form['name'], hashed_password, '0', '0'])
+        db.commit()
+
+        session['user'] = request.form['name']
+
+        return redirect(url_for('index'))
+
+    return render_template('register.html', user=user)
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    user = get_current_user()
+    error = None
+
+    if request.method == 'POST':
+        db = get_db()
+
+        name = request.form['name']
+        password = request.form['password']
+
+        user_cur = db.execute('select id, name, password from users where name = ?', [name])
+        user_result = user_cur.fetchone()
+
+        if user_result:
+
+            if check_password_hash(user_result['password'], password):
+                session['user'] = user_result['name']
+                return redirect(url_for('index'))
+            else:
+                error = 'The password is incorrect.'
+        else:
+            error = 'The username is incorrect'
+
+    return render_template('login.html', user=user, error=error)
+
+@app.route('/logout')
+def logout():
+    session.pop('user', None)
+    return redirect(url_for('index'))
+
+if __name__ == '__main__':
+    app.run(debug=True)
